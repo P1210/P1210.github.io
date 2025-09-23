9c2ff5944b2c4a6abcd0456b9a1883c8a1ee7b54
P1210 <gpranjal1210@gmail.com>
2025-09-23 21:07:53 +0530

    Updates

---
+++ _next/static/9Et3RqlXC5w8i_7Hx8ars/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/0c4ba8da013d651c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0c4ba8da013d651c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/7571dab5c35a62ac.js",
+  "static/chunks/turbopack-9da4401aec154ba5.js"
+])
